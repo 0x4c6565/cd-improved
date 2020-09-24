19ad215c053c88d4e82d8f2e5bcdbcc4cd2698cb
Lee Spottiswood <me@lee.io>
2020-09-24 22:43:19 +0100

    Update lol.ps1

--- lol.ps1
+++ lol.ps1
@@ -1,29 +1,31 @@
 $Env:OLDPWD = $null
 function Set-LocationImproved
 {
-    $CurrentDirectory = (Get-Location).Path
+    $CurrentOldPWD = $Env:OLDPWD
+    $Env:OLDPWD = (Get-Location).Path
 
     if ($args.Count -eq 0)
     {
         Set-Location -Path $HOME
-    }
-    elseif ($args[0] -eq "-")
-    {
-        if ($Env:OLDPWD -ne $null)
-        {
-            Set-Location -Path $Env:OLDPWD
-        }
-    }
-    elseif ($args[0] -is [string] -and ([string]$args[0]).StartsWith('~'))
-    {
-        Set-Location -Path ($args[0] -replace "^~\\*\/*","$HOME\")
-    }
-    else
-    {
-        Set-Location @args
+        return
     }
 
-    $Env:OLDPWD = $CurrentDirectory
+    if ($args[0] -eq "-")
+    {
+        if ($CurrentOldPWD -ne $null)
+        {
+            Set-Location -Path $CurrentOldPWD
+        }
+        return
+    }
+
+    if ($args[0] -is [string] -and ([string]$args[0]).StartsWith('~'))
+    {
+        Set-Location -Path ($args[0] -replace "^~\\*\/*", "$HOME\")
+        return
+    }
+
+    Set-Location @args
 }
 
 if (Test-Path -Path Alias:\cd)
